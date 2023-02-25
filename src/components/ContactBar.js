5ffb209d45fb7280a08b04d1d8e04d0d695f8c56
Ushindi Gedeon <gedeontshobohwa1@gmail.com>
2023-02-25 10:24:51 +0200

    Add contactbar to the page

---
+++ src/components/ContactBar.js
@@ -0,0 +1,20 @@
+import React from "react";
+import "./styles/contactBar.css"
+
+
+const ContactBar =()=>{
+    return <div className="contact-bar">
+        <div className="social-icon">Twitter</div>
+        <div className="social-icon">LinkedIn</div>
+        <div className="social-icon">Facebook</div>
+        <div className="social-icon">Dribble</div>
+        <div className="social-icon">GitHub</div>
+        <div className="social-icon">GitLab</div>
+        <div className="social-icon">Medium</div>
+        <div className="social-icon">Figma</div>
+        <div className="social-icon">Behance</div>
+      
+    </div>
+}
+
+export default ContactBar
