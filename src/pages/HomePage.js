c7ffe98661931f3ce314b1565ba7d90b2bff07a0
Ushindi Gedeon <gedeontshobohwa1@gmail.com>
2023-02-25 10:26:27 +0200

    Add homepage

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,8 +1,10 @@
 import React from "react";
+import "./styles/homepage.css";
+import Headline from "../components/Headline";
+import Profile from "../assets/profile.jpg"
 
 const HomePage = () => {
-    return <div>Home Page</div>;
-   
+    return <div className="homepage-container"><Headline/><div className="profile-image-section"><img src={Profile}  className="profile-image"/></div></div>
 };
 
 export default HomePage;
