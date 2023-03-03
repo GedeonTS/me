e04c08106e248008552cf3b20c218a98615a4f0f
Ushindi Gedeon <gedeontshobohwa1@gmail.com>
2023-03-03 19:07:19 +0200

    Add contact bar

--- src/components/ContactBar.js
+++ src/components/ContactBar.js
@@ -1,19 +1,27 @@
 import React from "react";
 import "./styles/contactBar.css"
 
+import Twitter from "../assets/socials/twitter.png"
+import LinkedIn from "../assets/socials/linkedin.png"
+import Facebook from "../assets/socials/facebook.png"
+import Dribble from "../assets/socials/dribble.png"
+import GitHub from "../assets/socials/github.png"
+import GitLab from "../assets/socials/gitlab.png"
+import Medium from "../assets/socials/medium.png"
+import Figma from "../assets/socials/figma.png"
+import Behance from "../assets/socials/behance.png"
 
 const ContactBar =()=>{
     return <div className="contact-bar">
-        <div className="social-icon">Twitter</div>
-        <div className="social-icon">LinkedIn</div>
-        <div className="social-icon">Facebook</div>
-        <div className="social-icon">Dribble</div>
-        <div className="social-icon">GitHub</div>
-        <div className="social-icon">GitLab</div>
-        <div className="social-icon">Medium</div>
-        <div className="social-icon">Figma</div>
-        <div className="social-icon">Behance</div>
-      
+        <div className="social-icon motion1"><img src={Twitter} style={{width:"50%"}}/></div>
+        <div className="social-icon motion2"><img src={LinkedIn} style={{width:"50%"}}/></div>
+        <div className="social-icon motion3"><img src={Facebook} style={{width:"50%"}}/></div>
+        <div className="social-icon motion4"><img src={Dribble} style={{width:"50%"}}/></div>
+        <div className="social-icon motion5"><img src={GitHub} style={{width:"50%"}}/></div>
+        <div className="social-icon motion6"><img src={GitLab} style={{width:"50%"}}/></div>
+        <div className="social-icon motion7"><img src={Medium} style={{width:"50%"}}/></div>
+        <div className="social-icon motion8"><img src={Figma} style={{width:"50%"}}/></div>
+        <div className="social-icon motion9"><img src={Behance} style={{width:"50%"}}/></div>
     </div>
 }
 
