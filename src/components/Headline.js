5a60489f4701e7c1e039ada4064a9ebd9421a320
Ushindi Gedeon <gedeontshobohwa1@gmail.com>
2023-03-03 19:07:33 +0200

    Add headline section to Homepage

--- src/components/Headline.js
+++ src/components/Headline.js
@@ -4,8 +4,12 @@ import "./styles/headline.css";
 const Headline = () => {
     return <div className="headline-container">
         <p className="my-text">Hello, I'm </p>
-        <h1 className="headline"><span className="name-wrapper">{"<"}</span> <span className="name">Ushindi Gedeon</span><span className="name-wrapper">{" />"}</span></h1>
-        <h2 className="job"> Web Developer</h2>
+        <h1 className="headline">
+            <span className="name-wrapper">{"<"}</span> 
+            <span className="name">Ushindi Gedeon</span>
+            <span className="name-wrapper">{" />"}</span>
+            </h1>
+        <h2 className="job">Web Developer</h2>
         <p className="my-text">I am ready to help you build your next big thing!</p>
     </div>;
 }
